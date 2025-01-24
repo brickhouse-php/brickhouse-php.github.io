56f4b39645f154557eed31b8d37d0a8801588a2f
Max T. Kristiansen <me@maxtrier.dk>
2025-01-24 19:55:42 +0100

    feat: initial docs commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,49 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Brickhouse",
+      social: {
+        github: "https://github.com/maxnatamo/brickhouse",
+      },
+      editLink: {
+        baseUrl: "https://github.com/brickhouse-php/docs/edit/main/",
+      },
+      sidebar: [
+        {
+          label: "Getting Started",
+          autogenerate: { directory: "getting-started" },
+        },
+        {
+          label: "Routing",
+          autogenerate: { directory: "routing" },
+        },
+      ],
+      expressiveCode: {
+        themes: ["github-dark", "github-light"],
+        styleOverrides: {
+          frames: {
+            editorActiveTabBackground: ["#1d293d", "#fdfdfd"],
+            editorActiveTabForeground: ["#7f96ac", "#5c7287"],
+            editorActiveTabIndicatorTopColor: ["#1d293d", "#fdfdfd"],
+            editorBackground: ["#1d293d", "#fdfdfd"],
+            editorTabBarBackground: ["#1d293d", "#fdfdfd"],
+            editorTabBarBorderBottomColor: ["#1d293d", "#fdfdfd"],
+            terminalBackground: ["#1d293d", "#fdfdfd"],
+            shadowColor: "transparent",
+          },
+        },
+      },
+      components: {
+        Pagination: "./src/components/Pagination.astro",
+      },
+      customCss: ["./src/styles/custom.css", "./src/tailwind.css"],
+    }),
+    tailwind(),
+  ],
 });
