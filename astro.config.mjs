6ae4e6251bc656bccf444a1297fe226f4b11d565
Max T. Kristiansen <me@maxtrier.dk>
2025-01-24 20:03:01 +0100

    ci: add Github Pages deployment flow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://brickhouse-php.github.io',
   integrations: [
     starlight({
       title: "Brickhouse",
